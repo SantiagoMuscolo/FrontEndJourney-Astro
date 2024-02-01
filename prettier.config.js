5940169472fdfdec38216fad8694cffb721d580b
Santi Muscolo <smuscolo2003@gmail.com>
2024-02-01 13:55:01 -0300

    navbar added

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  jsxBracketSameLine: false,
+  bracketSpacing: true,
+  printWidth: 100,
+  plugins: ['prettier-plugin-tailwindcss'],
+}
---
+++ src/constants/constants.ts
@@ -0,0 +1,14 @@
+export const navbarItems = [
+    {
+        id:1,
+        name: "about us"
+    },
+    {
+        id: 2,
+        name: "tips"
+    },
+    {
+        id: 3,
+        name: "Road Map"
+    }
+]
